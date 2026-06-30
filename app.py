cdab3fd48710ce3b170055660f71593346476849
A1K2A3N4 <as2545967@gmail.com>
2026-06-30 22:52:35 +0200

    Fix Streamlit deployment by importing matplotlib and adding requirements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from pathlib import Path
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
